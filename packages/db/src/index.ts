3402e71c5d40ccb3d7ece6431e500e13d1e6244e
bheemwartiz14-stack <bheem.wartiz14@gmail.com>
2026-06-17 18:32:48 +0530

    Fixed the mnain issue

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema/index";
 
-const databaseUrl = 'postgresql://neondb_owner:npg_zf8mlrkPgBR3@ep-red-snow-aqkv88xv-pooler.c-8.us-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require';
+const databaseUrl = process.env.DATABASE_URL;
 console.log('databaseUrl',databaseUrl);
 
 export const db = databaseUrl
